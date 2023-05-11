1c5b3543d36e969ebcb15e20aecd97f1103a18e6
zagui-master <github.correo@gmail.com>
2023-05-11 16:52:44 -0500

    first commit

---
+++ portafolio/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class project(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Titulo")
+    description = models.TextField(verbose_name="Descripcion")
+    picture = models.ImageField(verbose_name="Imagen", upload_to='projects')
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha creado")
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha actualizacion")
+
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
